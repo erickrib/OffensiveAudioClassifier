699ba82011f4a3bbc8704636e5b18d0c80c89c8c
Erick Ribeiro <erickrocha965@gmail.com>
2024-03-03 11:31:19 -0300

    feat: preparing cocoapods files

---
+++ Example/Pods/Target Support Files/OffensiveAudioClassifier/OffensiveAudioClassifier-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OffensiveAudioClassifier : NSObject
+@end
+@implementation PodsDummy_OffensiveAudioClassifier
+@end
---
+++ Example/Pods/Target Support Files/Pods-OffensiveAudioClassifier_Tests/Pods-OffensiveAudioClassifier_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OffensiveAudioClassifier_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_OffensiveAudioClassifier_Tests
+@end
